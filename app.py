b6d18f0bea2dbd81278db2442d8f6ebbcd4d5dc2
Linyan Cui <linyancui@LinyandeMac-mini.local>
2025-02-21 00:47:10 +0800

    Use gunicorn on port 3000

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from minimal Flask!"
+    return "Hello Gunicorn!"
 
-if __name__ == "__main__":
-    app.run(port=5000)
+#if __name__ == "__main__":
+#    app.run(port=5000)
